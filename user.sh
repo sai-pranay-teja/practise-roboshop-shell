d5bf4201c5e63621f282f964e84097ce826151b8
saipranayteja <pranayteja.ch@algonox.com>
2023-05-08 20:20:14 +0530

    sdfghj

---
+++ user.sh
@@ -0,0 +1,14 @@
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+yum install nodejs -y
+useradd roboshop
+mkdir /app
+curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
+cd /app
+unzip /tmp/user.zip
+cd /app
+npm install
+cp ${dir}/user.service /etc/systemd/system/user.service
+systemctl daemon-reload
+systemctl enable user
+systemctl start user
+mongo --host MONGODB-SERVER-IPADDRESS </app/schema/user.js
