68f2a0dfce259f6369da2389d18413d3e373343a
saipranayteja <pranayteja.ch@algonox.com>
2023-05-08 18:35:56 +0530

    zxc

--- instance-launch.sh
+++ instance-launch.sh
@@ -3,7 +3,7 @@ aws ec2 run-instances \
     --image-id ami-0b5a2b5b8f2be4ec2 \
     --instance-type t2.micro \
     --security-group-ids sg-036e9bfb37a180657 \
-    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=${component}}]' 'ResourceType=volume,Tags=[{Key=Name,Value=${component}}]' \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${component}}]' 'ResourceType=volume,Tags=[{Key=Name,Value=${component}}]" \
     --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"
 
 }
