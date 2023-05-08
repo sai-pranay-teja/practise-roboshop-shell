d2e90ea28849056703a7152bd8d6813ac81462a9
saipranayteja <pranayteja.ch@algonox.com>
2023-05-08 18:29:22 +0530

    xc

--- instance-launch.sh
+++ instance-launch.sh
@@ -4,7 +4,7 @@ aws ec2 run-instances \
     --instance-type t2.micro \
     --security-group-ids sg-036e9bfb37a180657 \
     --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=${component}}]' 'ResourceType=volume,Tags=[{Key=Name,Value=${component}}]' \
-    --instance-market-options MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}
+    --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"
 
 }
 
