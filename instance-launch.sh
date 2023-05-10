140b5280d1ade17d4f3be9e0f56794207c060b25
saipranayteja <pranayteja.ch@algonox.com>
2023-05-10 16:04:53 +0530

    SDCFV

--- instance-launch.sh
+++ instance-launch.sh
@@ -4,17 +4,17 @@ DOMAIN="practise-devops.online"
 create_all() { 
   echo -e '#!/bin/bash' >/tmp/user-data
   echo -e "\nset-hostname ${COMPONENT}" >>/tmp/user-data
-  PUBLIC_IP=$(aws ec2 run-instances \
+  PRIVATE_IP=$(aws ec2 run-instances \
     --image-id ami-0b5a2b5b8f2be4ec2 \
     --instance-type t2.micro \
     --security-group-ids sg-036e9bfb37a180657 \
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" "ResourceType=volume,Tags=[{Key=Name,Value=${COMPONENT}}]" \
     --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" \
     --user-data file:///tmp/user-data \
-    | jq '.Instances[].PublicIpAddress' | sed -e 's/"//g')
+    | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
   
   
-  sed -e "s/IPADDRESS/${PUBLIC_IP}/" -e "s/COMPONENT/${COMPONENT}/" -e "s/DOMAIN/${DOMAIN}/" route53.json > /tmp/record.json
+  sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}/" -e "s/DOMAIN/${DOMAIN}/" route53.json > /tmp/record.json
   aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json 2>/dev/null
 
   
