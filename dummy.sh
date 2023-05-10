154989500b76733082d77fc7ba9893503a2c5aa0
saipranayteja <pranayteja.ch@algonox.com>
2023-05-10 16:01:25 +0530

    sdcf

--- dummy.sh
+++ dummy.sh
@@ -3,10 +3,10 @@ aws ec2 run-instances \
     --instance-type t2.micro \
     --security-group-ids sg-036e9bfb37a180657 \
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=Random}]" "ResourceType=volume,Tags=[{Key=Name,Value=Random}]" \
-    --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" \
-    | jq '.Instances[].PublicIpAddress' | sed -e 's/"//g'
+    --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"
 
 
-PUBLIC_IP=$(aws ec2 describe-instances | jq '.Instances[].PublicIpAddress' | sed -e 's/"//g')
+
+PUBLIC_IP=$(aws ec2 describe-instances | jq 'Reservations[].Instances[].PublicIpAddress' | sed -e 's/"//g')
 
 echo $PUBLIC_IP
