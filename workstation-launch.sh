c2ac1cc3eef6ad8ca04214740cc64229cbb0eba0
saipranayteja <pranayteja.ch@algonox.com>
2023-05-10 13:45:19 +0530

    ccv

---
+++ workstation-launch.sh
@@ -0,0 +1,6 @@
+aws ec2 run-instances \
+    --image-id ami-0b5a2b5b8f2be4ec2 \
+    --instance-type t2.micro \
+    --security-group-ids sg-036e9bfb37a180657 \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=Workstation}]" "ResourceType=volume,Tags=[{Key=Name,Value=Workstation}]" \
+    --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"
