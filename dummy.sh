7af0a336def2292ca16cdabe24db7240289e5f8a
saipranayteja <pranayteja.ch@algonox.com>
2023-05-10 15:55:45 +0530

    sdcf

---
+++ dummy.sh
@@ -0,0 +1,7 @@
+aws ec2 run-instances \
+    --image-id ami-0b5a2b5b8f2be4ec2 \
+    --instance-type t2.micro \
+    --security-group-ids sg-036e9bfb37a180657 \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=Random}]" "ResourceType=volume,Tags=[{Key=Name,Value=Random}]" \
+    --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" \
+    | jq '.Instances[].PublicIpAddress' | sed -e 's/"//g'
--- instance-launch.sh
+++ instance-launch.sh
@@ -4,16 +4,16 @@ DOMAIN="practise-devops.online"
 create_all() { 
   echo -e '#!/bin/bash' >/tmp/user-data
   echo -e "\nset-hostname ${COMPONENT}" >>/tmp/user-data
-  aws ec2 run-instances \
+  PUBLIC_IP=$(aws ec2 run-instances \
     --image-id ami-0b5a2b5b8f2be4ec2 \
     --instance-type t2.micro \
     --security-group-ids sg-036e9bfb37a180657 \
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" "ResourceType=volume,Tags=[{Key=Name,Value=${COMPONENT}}]" \
     --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" \
-    --user-data file:///tmp/user-data 
+    --user-data file:///tmp/user-data \
+    | jq '.Instances[].PublicIpAddress' | sed -e 's/"//g')
   
-  PUBLIC_IP=$(aws ec2 describe-instances | jq '.Instances[].PublicIpAddress' | sed -e 's/"//g')
-  echo $PUBLIC_IP
+  
   sed -e "s/IPADDRESS/${PUBLIC_IP}/" -e "s/COMPONENT/${COMPONENT}/" -e "s/DOMAIN/${DOMAIN}/" route53.json > /tmp/record.json
   aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json 2>/dev/null
 
