00742fbfa36111db6e7d9ea73b9849cbe427bca3
saipranayteja <pranayteja.ch@algonox.com>
2023-05-10 13:13:30 +0530

    asdfg

--- instance-launch.sh
+++ instance-launch.sh
@@ -1,10 +1,19 @@
 create_all() {
-aws ec2 run-instances \
+  echo -e "\nset-hostname ${COMPONENT}" >>/tmp/user-data
+  PUBLIC_IP=$(aws ec2 run-instances \
     --image-id ami-0b5a2b5b8f2be4ec2 \
     --instance-type t2.micro \
     --security-group-ids sg-036e9bfb37a180657 \
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${component}}]" "ResourceType=volume,Tags=[{Key=Name,Value=${component}}]" \
     --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"
+    | jq '.Instances[].PublicIpAddress' | sed -e 's/"//g')
+
+# aws ec2 run-instances \
+#     --image-id ami-0b5a2b5b8f2be4ec2 \
+#     --instance-type t2.micro \
+#     --security-group-ids sg-036e9bfb37a180657 \
+#     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${component}}]" "ResourceType=volume,Tags=[{Key=Name,Value=${component}}]" \
+#     --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"
 
 }
 
