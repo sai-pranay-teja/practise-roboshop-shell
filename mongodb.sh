9ce90f96260e860315e6ba16af14cc33686e0e62
saipranayteja <pranayteja.ch@algonox.com>
2023-05-11 23:40:40 +0530

    wsdf

--- mongodb.sh
+++ mongodb.sh
@@ -1,7 +1,8 @@
 source common.sh
 
 yum install mongodb-org -y
-systemctl enable mongod
 cp ${dir}/mongo.repo /etc/yum.repos.d/mongo.repo
+systemctl enable mongod
+sed -e -i 's/127.0.0.1/0.0.0.0' /etc/mongod.conf
 systemctl start mongod
 systemctl restart mongod
