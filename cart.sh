172d62418b7b2350027b7811e39ed1ae87b8f000
saipranayteja <pranayteja.ch@algonox.com>
2023-05-12 00:54:05 +0530

    cvc

--- cart.sh
+++ cart.sh
@@ -7,8 +7,7 @@ useradd roboshop
 mkdir /app
 curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
 cd /app 
-unzip /tmp/cart.zip
-cd /app 
+unzip /tmp/cart.zip 
 npm install 
 cp ${dir}/cart.service /etc/systemd/system/cart.service
 systemctl daemon-reload
