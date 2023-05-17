9f1fff833aa5461e141f4d6dbbec46e3379a4df7
saipranayteja <pranayteja.ch@algonox.com>
2023-05-17 10:42:00 +0530

    zxcv

---
+++ test.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+  region = "us-east-1"
+  
+}
+
+resource "aws_spot_instance_request" "name_2" {
+    ami           = "ami-0bb6af715826253bf"
+    instance_type = "t3a.small"
+    spot_type     = "persistent"
+    #iam_instance_profile = aws_iam_instance_profile.demo-profile.name
+    instance_interruption_behavior = "stop"
+    wait_for_fulfillment=true
+    vpc_security_group_ids = ["sg-036e9bfb37a180657"]
+    tags = {
+      Name = "Instance-1"
+    }
+
+    timeouts {
+    create = "10m"
+    delete = "10m"
+  }
+
+}
+
+output "public_ip"{
+    value=aws_spot_instance_request.name_2.public_ip
+}
