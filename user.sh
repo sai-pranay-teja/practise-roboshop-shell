6180dd225a9c55627abc40fa6561455c547b0e86
saipranayteja <pranayteja.ch@algonox.com>
2023-05-18 05:49:14 +0530

    asdf

--- user.sh
+++ user.sh
@@ -5,19 +5,3 @@ db_type="mongo"
 nodejs
 
 
-
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-yum install nodejs -y
-useradd roboshop
-mkdir /app
-curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
-cd /app
-unzip /tmp/user.zip
-npm install
-cp ${dir}/user.service /etc/systemd/system/user.service
-systemctl daemon-reload
-systemctl enable user
-systemctl start user
-cp ${dir}/mongo.repo /etc/yum.repos.d/mongo.repo
-yum install mongodb-org-shell -y
-mongo --host mongodb.practise-devops.online </app/schema/user.js
